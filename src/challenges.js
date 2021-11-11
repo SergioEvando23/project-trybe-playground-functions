fc661bbf217dd46b52cabcd896f571ed12aa4134
Sérgio Costa <sergioevandocosta@gmail.com>
2021-11-11 17:11:16 -0300

    quetões 4 e 5 completas

--- src/challenges.js
+++ src/challenges.js
@@ -18,6 +18,7 @@ calcArea(5, 2);
 calcArea(51, 1);
 
 // Desafio 3
+
 function splitSentence(string, separetor) {
   let arrayString = string.split(separetor);
   return arrayString;
@@ -27,36 +28,49 @@ splitSentence("vamo que vamo", " ");
 splitSentence("foguete", " ");
 
 // Desafio 4
-function concatName() {
- 
+let concatNameArray1 = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+let concatNameArray2 = ['foguete', 'não', 'tem', 'ré'];
+let concatNameArray3 = ['captain', 'my', 'captain'];
+
+function concatName(array) {
+  let fristName = array[0];
+  let lastName = array[array.length - 1];
+  return lastName.concat(', ', fristName);
 }
+concatName(concatNameArray1);
+concatName(concatNameArray2);
+concatName(concatNameArray3);
 
 // Desafio 5
-function footballPoints() {
- 
+function footballPoints(wins, ties) {
+  let points = (wins * 3) + (ties);
+  return points;
 }
+footballPoints(14, 8);
+footballPoints(1, 2);
+footballPoints(0, 0);
 
 // Desafio 6
 function highestCount() {
- 
+  
 }
 
 // Desafio 7
 function catAndMouse() {
- 
+  
 }
 
 // Desafio 8
 function fizzBuzz() {
- 
+  
 }
 
 // Desafio 9
 function encode() {
- 
+  
 }
 function decode() {
- 
+  
 }
 
 module.exports = {
@@ -71,3 +85,5 @@ module.exports = {
   highestCount,
   splitSentence,
 };
+
+// referencia de pesquisa, documentação MDN  (https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String)
