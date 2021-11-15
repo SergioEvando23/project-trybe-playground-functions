aacceb3f9e3df907884fe25d318ab081a8723c65
Sérgio Costa <sergioevandocosta@gmail.com>
2021-11-15 14:00:10 -0300

    erros de lint corrigidos

--- src/challenges2.js
+++ src/challenges2.js
@@ -5,14 +5,14 @@ function techList() {
 
 // Desafio 11
 function generatePhoneNumber() {
- 
+
 }
 
 // Desafio 12
 function triangleCheck(measureA, measureB, measureC) {
-  if (measureA > measureB + measureC 
-      || measureB > measureA + measureC 
-      || measureC > measureA + measureB ) {
+  if (measureA > measureB + measureC
+      || measureB > measureA + measureC
+      || measureC > measureA + measureB) {
     return false;
   }
   return true;
@@ -21,7 +21,7 @@ triangleCheck(10, 14, 8);
 
 // Desafio 13
 function hydrate() {
- 
+
 }
 
 module.exports = {
